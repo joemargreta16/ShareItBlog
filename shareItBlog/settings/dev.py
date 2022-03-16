b801e796ab1499946821c0a353c95a1dba9b5b1c
Greta <joemar.greta16@gmail.com>
2022-03-16 12:01:07 +0800

    Debugging some settings for development and production.

--- shareItBlog/settings/dev.py
+++ shareItBlog/settings/dev.py
@@ -18,7 +18,10 @@ ALLOWED_HOSTS = ['*']
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
